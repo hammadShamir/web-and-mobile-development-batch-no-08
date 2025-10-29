f9db9b02e5c0116b3ad9542d30c197db70b10e85
Hammad Shamir <103999866+hammadShamir@users.noreply.github.com>
2025-10-29 10:05:59 +0500

    Add files via upload

---
+++ Js-variables-lecture-2/index.js
@@ -0,0 +1,27 @@
+'use strict';
+// var name = "hammad";
+// var name = "Shamir";
+// console.log(name);
+// age = 22;
+// console.log(age);
+
+// let city = "Hyderabad";
+// city = "karachi";
+// let city = "karachi"
+// console.log(city);
+
+const email = "hammad@gmail.com"
+const Email = "example"
+// email = "example@gmail.com"
+console.log(Email);
+
+// const product1;
+// const product2_name;
+// const 1product
+// const product_name
+// const productName 
+
+const name = "Ali";
+const age = "22";
+console.log(name + " " + age)
+
