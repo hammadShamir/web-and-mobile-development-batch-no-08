64a9584ae37d05a1c815698c769e1fdeb8b73d75
hammadShamir <hammadshamir7@gmail.com>
2025-11-12 10:58:39 +0500

    Feat(JavaScript): Add if/else

---
+++ JS-conditional-statements-5/index.js
@@ -0,0 +1,42 @@
+// let isRaining = true;
+
+// if(isRaining){
+//     console.log("Take an Umbrella");
+// }else{
+//     console.log("Go without it");
+
+// }
+
+
+// let age = 14;
+
+// if(age >= 18){
+//     console.log("Take you NIC");
+// }else{
+//     console.log("you are under age");
+// }
+
+
+// let marks = 91;
+
+// if (marks >= 90) {
+//     console.log("Grade: A+");
+// } else if (marks >= 80) {
+//     console.log("Grade: A");
+// } else{
+//     console.log("HEo");
+// }
+
+
+let age = 14;
+let hasLicense = true;
+
+if (age >= 18) {
+  if (hasLicense) {
+    console.log("You can drive!");
+  } else {
+    console.log("Get your driving license first!");
+  }
+} else {
+  console.log("You are too young to drive!");
+}
