6228953cc287731bb13aa0985e12469ff0e69538
hammadShamir <hammadshamir7@gmail.com>
2025-11-17 10:03:52 +0500

    feat(Conditional): add switch case

---
+++ Js-switch-statements-6/index.js
@@ -0,0 +1,31 @@
+let marks = 30;
+
+switch (marks) {
+    case 90:
+        console.log("Grade A+");
+        break;
+    case 80:
+        console.log("Grade A");
+        break;
+    case 60:
+        console.log("Grade B");
+        break;
+    case 40:
+        console.log("Grade C");
+    default:
+        console.log("Fail");
+        break;
+}
+
+
+let username = "admin";
+let password = "psa"
+
+switch (username && password) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
