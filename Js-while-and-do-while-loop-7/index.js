2707e149153c42b2d6aa34c224e1d2753a9a2ad9
hammadShamir <hammadshamir7@gmail.com>
2025-11-19 10:19:45 +0500

    feat(Loop): Add while and do while loop

---
+++ Js-while-and-do-while-loop-7/index.js
@@ -0,0 +1,25 @@
+// let busArrived = false;
+
+// while (!busArrived) {
+//      console.log("⏳ Waiting for the bus...");
+//     let randomNum = Math.random();
+//     busArrived = randomNum > 0.7;
+// }
+
+// console.log("🚌 Bus has arrived!");
+
+// let correctPin = 1234;
+// let enteredPin;
+
+// do {
+//     enteredPin = +prompt("Enter your PIN:");
+// } while (enteredPin !== correctPin);
+
+// alert("✅ Access Granted");
+
+
+for (let i = 1; i <= 10; i++) {
+  if (i === 5) break;
+  console.log(i);
+}
+console.log("helo");
