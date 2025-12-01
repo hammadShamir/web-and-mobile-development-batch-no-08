9b601073ddd27309bee5ad7e6f9d570c5f510915
hammadShamir <hammadshamir7@gmail.com>
2025-12-01 10:12:35 +0500

    feat(JavaScript): Add Arrays docs

---
+++ JS-arrays-9/index.js
@@ -0,0 +1,17 @@
+// let fruits = ["Apple", "Mango", "Banana", "Orange", "Grapes"];
+
+// for (let index = 0; index < fruits.length; index++) {
+//     console.log(fruits[index])
+// }
+// fruits.push("Strawberry")
+// fruits.pop();
+// for (let fruit of fruits) {
+//   console.log(fruit);
+// }
+// console.log(fruits.indexOf("Strawberry"))
+// const isBanana = fruits.includes("Banana")
+let fruits = ["Apple", "Mango", "Banana", "Orange", "Grapes"];
+
+let citrus = fruits.slice(2,5);
+
+console.log(citrus)
