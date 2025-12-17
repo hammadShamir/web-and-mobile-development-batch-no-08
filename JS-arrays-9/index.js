895c41a0a6cf33166dc125685066912cd8a4cbda
hammadShamir <hammadshamir7@gmail.com>
2025-12-17 10:12:07 +0500

    feat(array)

--- JS-arrays-9/index.js
+++ JS-arrays-9/index.js
@@ -10,8 +10,74 @@
 // }
 // console.log(fruits.indexOf("Strawberry"))
 // const isBanana = fruits.includes("Banana")
-let fruits = ["Apple", "Mango", "Banana", "Orange", "Grapes"];
+// let fruits = ["Apple", "Mango", "Banana", "Orange", "Grapes"];
+// let number = [99,49]
+// let obj = [
+//     {name:'hammad', phone: 0, },
+//     {name:'noman', phone:1}
+// ]
 
-let citrus = fruits.slice(2,5);
+// let arr = [
+//     ['Haseeb', 'tahir'],
+//     ['umer', 'moid'],
+// ]
 
-console.log(citrus)
+// let students = [
+//     {
+//         name: "Moid",
+//         course: "Web And App",
+//         status: true,
+//         feesStatus: false
+//     },
+//     {
+//         name: "Ahmed",
+//         course: "Web And App",
+//         status: true,
+//         feesStatus: false
+//     },
+// ]
+
+// const list = document.createElement('ul');
+// document.body.appendChild(list);
+// for (let index = 0; index < students.length; index++) {
+//     const student = students[index];
+//     const item = document.createElement('li');
+
+//     item.textContent = student.name;
+//     list.appendChild(item);
+// }
+
+
+// students.forEach(element => {
+
+// });
+
+let nums = [1, 2, 3];
+
+let doubled = nums.map(n => n * 2);
+
+console.log(doubled);
+console.log(nums);
+// for (let index = 0; index < arr.length; index++) {
+//     const element = arr[index];
+//     for (let y = 0; y < element.length; y++) {
+//         const elementY = element[y];
+//         if (elementY === "umer") break;
+//         console.log(elementY)
+//     }
+// }
+
+// let multivalues = [
+//     'ddd',
+//     20,
+//     {
+//         name: 'heee'
+//     },
+//     [
+
+//     ]
+// ]
+
+// let citrus = fruits.slice(2,5);
+
+// console.log(citrus)
