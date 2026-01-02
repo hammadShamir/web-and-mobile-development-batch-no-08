998043dd7686efa99503ec8cd8d333d09e22ec3c
hammadShamir <hammadshamir7@gmail.com>
2026-01-02 10:49:58 +0500

    Promises

--- Js-async/index.js
+++ Js-async/index.js
@@ -32,19 +32,54 @@
 
 // }, 1000);
 
-const myPromise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-        let success = false;
-        if (success) {
-            resolve("Success")
-        } else {
-            reject("Fail")
-        }
-    }, 1000)
-})
+// const myPromise = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         let success = false;
+//         if (success) {
+//             resolve("Success")
+//         } else {
+//             reject("Fail")
+//         }
+//     }, 1000)
+// })
 
-myPromise.then((res)=>{
-    console.log(res)
-}).catch((err)=>{
-    console.log(err)
-})
+// myPromise.then((result)=>{
+//     console.log(result)
+// }).catch((error)=>{
+//     console.log(error)
+// }).finally(()=>{
+//     console.log("Run with Fail and sucess")
+// })
+
+
+// function sum(num1, num2) {
+//     const sums = num1 + num2
+//     return sums
+// }
+
+// const answer = sum(1, 1);
+
+// console.log(answer)
+
+// function step1() {
+//     return new Promise((resolve) => {
+//         setTimeout(() => resolve("Step 1 done"), 1000);
+//     });
+// }
+
+// function step2(message) {
+//     return new Promise((resolve) => {
+//         setTimeout(() => resolve(message + " → Step 2 done"), 1000);
+//     });
+// }
+
+// step1()
+//     .then(step2)
+//     .then((result) => console.log(result))
+//     .catch((err) => console.error(err));
+
+
+fetch('https://pokeapi.co/api/v2/pokemon')
+    .then((response) => response.json())
+    .then((data) => console.log(data))
+    .catch((error) => console.error(error));
